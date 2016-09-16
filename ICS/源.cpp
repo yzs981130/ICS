edda5690342aa9089b1bb5e78996c061eb81e71a
叶志晟 <yzs981130@126.com>
2016-09-16 21:03:08 +0800

    添加项目文件。

---
+++ ICS/源.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+unsigned replace_byte(unsigned x, unsigned char b, int i)
+{
+	return (x & ~(0xFF << (i << 3))) | (b << (i << 3));
+}
+int func(int x)
+{
+	return (!(~x)) | !x | !~(x >> ((sizeof(int) - 1) << 3)) | !(x & 0xFF);
+}
+int even_ones(unsigned x)
+{
+	int a = (x >> 16) ^ x;
+	x = (a >> 8) ^ a;
+	a = (x >> 4) ^ x;
+	x = (a >> 2) ^ a;
+	a = (x >> 1) ^ x;
+	return (a & 1);
+}
+int main()
+{
+	cout << hex << replace_byte(0x12345678, 0xAB, 2);
+	system("pause");
+	return 0;
+}
