7d526732eabd74d2c6581a7dd1e3c133bdd1f982
叶志晟 <yzs981130@126.com>
2016-09-16 21:07:51 +0800

    2.60 2.61 2.65

--- ICS/源.cpp
+++ ICS/源.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 using namespace std;
+
+//2.60
 unsigned replace_byte(unsigned x, unsigned char b, int i)
 {
 	return (x & ~(0xFF << (i << 3))) | (b << (i << 3));
 }
+
+//2.61
 int func(int x)
 {
 	return (!(~x)) | !x | !~(x >> ((sizeof(int) - 1) << 3)) | !(x & 0xFF);
 }
+
+//2.65
 int even_ones(unsigned x)
 {
 	int a = (x >> 16) ^ x;
@@ -17,6 +23,7 @@ int even_ones(unsigned x)
 	a = (x >> 1) ^ x;
 	return (a & 1);
 }
+
 int main()
 {
 	cout << hex << replace_byte(0x12345678, 0xAB, 2);
